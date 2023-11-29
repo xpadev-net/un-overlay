dbaf328d3d8ac989966c8d8e5542f5cbc330c44e
XPA <xpadev@gmail.com>
2023-11-29 23:14:07 +0900

    feat: desktop overlay

---
+++ Assets/Scripts/DesktopOverlayManager.cs
@@ -0,0 +1,73 @@
+using EasyLazyLibrary;
+using UnityEngine;
+using uWindowCapture;
+
+public class DesktopOverlayManager : MonoBehaviour
+{
+    public int desktopIndex = 0;
+    private GameObject desktopCapture;
+    private Transform desktopCaptureTransform;
+    private GameObject canvas;
+    private RectTransform canvasRectTransform;
+    private UwcWindowTexture windowTexture;
+    private RenderTexture targetTexture;
+    private GameObject overlayCameraObj;
+    private Camera overlayCamera;
+    private GameObject overlaySystem;
+    private EasyOpenVROverlayForUnity easyOverlay;
+    private string overlayId;
+    
+    private int windowWidth;
+    private int windowHeight;
+    private bool initialized;
+    public static DesktopOverlayManager Init(int desktopIndex)
+    {
+        var prefab = Resources.Load<GameObject>("DesktopOverlayManager");
+        var manager = Instantiate(prefab).GetComponent<DesktopOverlayManager>();
+        manager.desktopIndex = desktopIndex;
+        return manager;
+    }
+    
+    void Start()
+    {
+        overlayId = "dev.xpa.steamvr-overlay."+Uuid.GetUuid();
+        initialized = false;
+        desktopCapture = transform.Find("DesktopCapture").gameObject;
+        windowTexture = desktopCapture.GetComponent<UwcWindowTexture>();
+        desktopCaptureTransform = desktopCapture.GetComponent<Transform>();
+        canvas = transform.Find("Canvas").gameObject;
+        canvasRectTransform = canvas.GetComponent<RectTransform>();
+        overlayCameraObj = transform.Find("OverlayCamera").gameObject;
+        overlayCamera = overlayCameraObj.GetComponent<Camera>();
+        overlaySystem = transform.Find("OverlaySystem").gameObject;
+        easyOverlay = overlaySystem.GetComponent<EasyOpenVROverlayForUnity>();
+        easyOverlay.OverlayKeyName = overlayId;
+        easyOverlay.OverlayFriendlyName = overlayId;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initialized)
+        {
+            if (!windowTexture||windowTexture.window==null)
+            {
+                return;
+            }
+            initialized = true;
+            windowWidth = windowTexture.window.width;
+            windowHeight = windowTexture.window.height;
+            desktopCaptureTransform.localScale = new Vector3(windowWidth, windowHeight);
+            targetTexture = new RenderTexture(windowWidth,windowHeight,32);
+            overlayCamera.targetTexture = targetTexture;
+            easyOverlay.renderTexture = targetTexture;
+            canvasRectTransform.sizeDelta = new Vector2(windowWidth, windowHeight);
+            windowTexture.desktopIndex = desktopIndex;
+        }
+
+        if (windowTexture.desktopIndex != desktopIndex)
+        {
+            windowTexture.desktopIndex = desktopIndex;            
+        }
+    }
+}
---
+++ Assets/Scripts/uuid.cs
@@ -0,0 +1,8 @@
+﻿public class Uuid
+{
+    public static string GetUuid()
+    {
+        var guid = System.Guid.NewGuid();
+        return guid.ToString();
+    }
+}
